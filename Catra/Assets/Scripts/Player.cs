d1f6f385e517c22bb59bc04ac0c49b214ca92ee5
ceenbag <yorkconnor03@gmail.com>
2026-03-07 23:53:32 -0500

    added spikes, tiles, and the ability to take damage

--- Catra/Assets/Scripts/Player.cs
+++ Catra/Assets/Scripts/Player.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,16 +7,22 @@ public class Player : MonoBehaviour
 {
     [Header("Player Component References")]
     [SerializeField] Rigidbody2D rb;
+    [SerializeField] SpriteRenderer spriteRenderer;
+    [SerializeField] Animator animator;
 
     [Header("Player Settings")]
+    [SerializeField] int health;
     [SerializeField] float speed;
     [SerializeField] float jumpingPower;
+    [SerializeField] int extraJumpsValue;
 
     [Header("Gounding")]
     [SerializeField] LayerMask groundLayer;
     [SerializeField] Transform groundCheck;
 
+
     private float horizontal;
+    private int extraJumps;
 
     private void FixedUpdate()
     {
@@ -27,14 +35,52 @@ public class Player : MonoBehaviour
 
     public void Jump(InputAction.CallbackContext context)
     {
-        if(context.performed && IsGrounded())
+        if(IsGrounded())
         {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
+            extraJumps = extraJumpsValue;
+        }
+        if(context.performed)
+        {
+            if(IsGrounded())
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
+            }
+            else if(extraJumps > 0)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
+                extraJumps--;
+            }
         }
     }
 
     private bool IsGrounded()
     {
-        return Physics2D.OverlapCapsule(groundCheck.position, new Vector2(1f, 0.1f), CapsuleDirection2D.Horizontal, 0, groundLayer);
+        return Physics2D.OverlapCapsule(groundCheck.position, new Vector2(1f, 0.1f), CapsuleDirection2D.Horizontal, 0.2f, groundLayer);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Damage")
+        {
+            health -= 25;
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 4);
+            StartCoroutine(BlinkRed());
+
+            if(health <= 0)
+            {
+                Die();
+            }
+        }
+    }
+    private IEnumerator BlinkRed()
+    {
+        spriteRenderer.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        spriteRenderer.color = Color.white;
+    }
+
+    private void Die()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Level1");
     }
 }
