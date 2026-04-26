64fb8576eb045e17c33cda4c11cf5d4853dacc87
ceenbag <yorkconnor03@gmail.com>
2026-04-26 17:54:04 -0400

    Added Iframes, Platforming over pit, and another enemy.

--- Catra/Assets/Scripts/Health.cs
+++ Catra/Assets/Scripts/Health.cs
@@ -1,16 +1,25 @@
+using System.Collections;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [Header ("Health")]
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
     private Animator anim;
     private bool dead;
 
+    [Header("iFrames")]
+    [SerializeField] private float iFrameDuration;
+    [SerializeField] private int numberOfFlashes;
+    private SpriteRenderer spriteRend;
+
+
     private void Awake()
     {
         currentHealth = startingHealth;
         anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
     }
     public void TakeDamage(float _damage)
     {
@@ -19,7 +28,7 @@ public class Health : MonoBehaviour
         if (currentHealth > 0)
         {
             anim.SetTrigger("hurt");
-            //iframes
+            StartCoroutine(Invulnerability());
         }
         else
         {
@@ -30,6 +39,19 @@ public class Health : MonoBehaviour
                 dead = true;
             }
         }
+    }
+    private IEnumerator Invulnerability()
+    {
+        Physics2D.IgnoreLayerCollision(10, 11, true);
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(1);
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(1);
+        }
+
+        Physics2D.IgnoreLayerCollision(10, 11, false);
     }
 
 }
